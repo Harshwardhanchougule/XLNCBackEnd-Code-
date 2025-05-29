21fede932a9a7053052eaf59ee5845c4031195f8
Harshwardhanchougule <harshchougule25@gmail.com>
2025-05-29 13:13:46 +0530

    Add file DB

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,19 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGO_URI as string;
 
     if (!mongoURI) {
-      throw new Error('MONGO_URI is not defined in .env');
+      throw new Error("MONGO_URI is not defined in .env");
     }
 
     await mongoose.connect(mongoURI);
 
-    console.log('✅ MongoDB Connected Successfully');
+    console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
-    console.error('❌ MongoDB Connection Error:', error);
+    console.error("❌ MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
+
 export default connectDB;
